f704590427c733d146b356e9dd1c0c5ee2bfbf7d
cloudy <im@joe.cloud>
2018-03-15 14:02:05 -0500

    Add tester

---
+++ InverseModelTest.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+# Script Test the Inverse Kineamtic Model Estimator
+
+# import libraries
+import numpy as np
+import math
+import intera_interface
+import rospy
+import roslib
+import tensorflow
+from keras.models import load_model
+
+# Initialize a ROS node 
+rospy.init_node('inverse_model_testing')
+
+# Initialize the Limb object
+limb=intera_interface.Limb('right')
+
+# Array to store the joint names
+joint_names = ['right_j6','right_j5','right_j4','right_j3','right_j2','right_j1','right_j0']
+#p_cube=np.array([[0.187,-0.462,-0.07]])
+p_cube=np.array([[0,-0.6,-0.07]])
+
+
+# Models location
+forward_model_file='weights/ForwardModel.h5'
+inverse_model_file='weights/InverseModelCombined.h5'
+
+# Load the model
+forwardModel = load_model(forward_model_file)
+InverseModel = load_model(inverse_model_file)
+
+# Review of the Model
+forwardModel.summary()
+InverseModel.summary()
+
+# Move the Robot to the starting location
+angles = dict()
+angles['right_j0']=math.radians(0)
+angles['right_j1']=math.radians(-50)
+angles['right_j2']=math.radians(0)
+angles['right_j3']=math.radians(120)
+angles['right_j4']=math.radians(0)
+angles['right_j5']=math.radians(0)
+angles['right_j6']=math.radians(0)
+limb.move_to_joint_positions(angles)
+
+# Grab the position of the joints
+joint_positions=limb.joint_angles()
+
+# Convert them to an np array
+joint_names = ['right_j0','right_j1','right_j3','right_j5','right_j6']
+q=np.array([[float(joint_positions[i]) for i in joint_names]])
+
+# Perform a prediction
+prediction=forwardModel.predict(q)
+
+# Define the state for the first time
+prev_state = prediction
+prev_q=q
+
+# Define the velocity dictionary
+velocities=dict()
+
+# Define dt
+dt=0.4
+
+while not rospy.is_shutdown():
+
+	# Grab the position of the joints
+	joint_positions=limb.joint_angles()
+
+	# Convert them to an np array
+	q=np.array([[float(joint_positions[i]) for i in joint_names]])
+
+	print q
+
+	# Perform a prediction
+
+	prediction=forwardModel.predict(q)
+	prediction_combined = np.hstack((prev_state - p_cube, prediction - p_cube))
+	joint_estimation = InverseModel.predict(prediction_combined)
+
+	print joint_estimation
+	print '-----------------'
+
+	print prev_state.shape
+	print joint_estimation.shape
+	#Push the velocities to the robot
+	velocities['right_j0']=(joint_estimation[0][0]-prev_q[0][0])/dt
+	velocities['right_j1']=(joint_estimation[0][1]-prev_q[0][1])/dt
+	velocities['right_j2']=0
+	velocities['right_j3']=(joint_estimation[0][2]-prev_q[0][2])/dt
+	velocities['right_j4']=0
+	velocities['right_j5']=(joint_estimation[0][3]-prev_q[0][3])/dt
+	velocities['right_j6']=(joint_estimation[0][4]-prev_q[0][4])/dt
+
+
+	limb.set_joint_velocities(velocities)
+
+
+	# The current prediction becomes the previous state during the next iteration
+	prev_state = prediction
+	prev_q=q
+
--- InverseModelTraining.py
+++ InverseModelTraining.py
@@ -1,155 +1,132 @@
 #!/usr/bin/env python
 
-# Script to Test Keras
-
-# import libraries
-import os
 import glob
 import numpy as np
-import sympy as sp
-import math
-#import rospy
-#import roslib
-import tensorflow
-from keras.models import Sequential
-from keras.layers import Activation
-from keras.layers import Dense
+from operator import itemgetter
+from keras.models import Sequential, load_model
+from keras.layers import Activation, Dense
 from keras import optimizers
-from keras.models import load_model
-from sympy import lambdify
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
-def get_position(matrix):
-
-	#print matrix
-
-	return [matrix[0][3],matrix[1][3],matrix[2][3]]
-
-
-# Forward Model location
-model_file='weights/ForwardModel.h5'
+#training_files = sorted(glob.glob('data/data_*'))
+training_files = ['data/data_5']
+model_file = 'weights/ForwardModel.h5'
+inverse_model_file = 'weights/InverseModelCombined.h5'
 
 # Load the model
 forwardModel = load_model(model_file)
 
-# Define the paths of the data
-#training_file='/home/michail/ros_ws/src/intera_sdk/intera_examples/scripts/InverseModelData/data5.txt'
-#training_path = '/home/michail/ros_ws/src/intera_sdk/intera_examples/scripts/InverseModelData/data_*'
-#training_files = glob.glob(training_path)
-#training_files = ['/home/michail/ros_ws/src/intera_sdk/intera_examples/scripts/InverseModelData/data_5', 
-#					'/home/michail/ros_ws/src/intera_sdk/intera_examples/scripts/InverseModelData/data_6', 
-#					'/home/michail/ros_ws/src/intera_sdk/intera_examples/scripts/InverseModelData/data_7', 
-#					'/home/michail/ros_ws/src/intera_sdk/intera_examples/scripts/InverseModelData/data_8']
-training_files = ['data/data_5', 'data/data_6','data/data_7', 'data/data_8']  
+# Load each data file into quasi-3D structure, note, since axis1 varies in length
+# it is a list of 2D numpy arrays, this is resolved below with padding
+training_data = [np.loadtxt(f ,delimiter=',',skiprows=1) for f in training_files]
 
-# Define where you want the Inverse model to be stored
-#inverse_model_file='/home/michail/ros_ws/src/intera_sdk/intera_examples/scripts/InverseModel/InverseModelCombined.h5'
-inverse_model_file='weights/InverseModelCombined.h5'
+for i in range(len(training_data)):
+    print training_files[i], ":", training_data[i].shape 
+    
+# Create blank 2D array with 'outside' shape equal to that of data
+input_training_data_n = [[]]*len(training_data)
 
+# Get max length, to pad the other trajectories with 0s
+max_len = max([td.shape for td in training_data],key=itemgetter(0))[0]
 
-# Create the training dataset
-#training_data= np.loadtxt(training_files[0],delimiter=',',skiprows=1)
+# Cube positions
+p_cube1 = [0.18529411764705872, -0.46270270270270264,-0.07]
+p_cube2 = [-0.17867647058823533, -0.47783783783783784,-0.07]
+p_cube3 = [0.1874999999999999, -0.6983783783783784,-0.07]
+p_cube4 = [-0.17647058823529416, -0.7027027027027026,-0.07]
 
-training_data = np.array([np.loadtxt(file,delimiter=',',skiprows=1) for file in training_files])
-
-
-# Dataset to learn the Inverse kinematics of the Sawyer Robot.
-
-# Declate some empty array lists
-input_training_data_r=np.empty((4,0,3),float)
-input_training_data_n= [] #np.empty((0,3),float)
-
-# Position of the cube
-#p_cube=np.array([[0.187,-0.462,-0.07]])
-
-p_cube1 = np.array([0.18529411764705872, -0.46270270270270264,-0.07])
-p_cube2 = np.array([-0.17867647058823533, -0.47783783783783784,-0.07])
-p_cube3 = np.array([0.1874999999999999, -0.6983783783783784,-0.07])
-p_cube4 = np.array([-0.17647058823529416, -0.7027027027027026,-0.07])
+# Cube order matches that of trajectories in data files
 p_cubes = np.array([p_cube1, p_cube3, p_cube2, p_cube4])
 
+# Delete columns for time, frozen joints, gripper
 out_tr_dat = [np.delete(data,[0,3,7,8],axis=1) for data in training_data]
-input_training_data_n = np.array([np.array([ forwardModel.predict(np.array([i])) -p_cubes[j] for i in out_tr_dat[j]]) for j in range(len(training_data))])
+
+# Build input data for Inverse Model based on Forward Model predictions
+for i in range(len(training_files)):
+    input_training_data_n[i] = []
+    for j in out_tr_dat[i]:
+        prediction = forwardModel.predict(np.array([j]))
+        input_training_data_n[i].append(prediction - p_cubes[i])
+    # Concate to reduce redundant dimension, i.e. [[x,y,z]] -> [x,y,z]
+    input_training_data_n[i] = np.copy(np.concatenate(input_training_data_n[i]))
+    # Pads trajectories with less steps than the highest count trajectory, pads bottom with zero
+    input_training_data_n[i] = np.pad(input_training_data_n[i], 
+                                      ((0, max_len - len(input_training_data_n[i])), (0,0)), 'constant')
+
+for i in range(len(out_tr_dat)):
+    out_tr_dat[i] = np.pad(out_tr_dat[i], ((0, max_len - len(out_tr_dat[i])), (0,0)), 'constant')
+
+out_tr_dat = np.array(out_tr_dat)
+input_training_data_n = np.array(input_training_data_n)
 
 
-print(training_data[0])
-"""
-for index, data in enumerate(training_data):
-	# Create the labels of the training data
-	output_training_data=np.delete(data,[0,3,7,8],axis=1)
-
-	for i in output_training_data:
-
-		# Get the correct joint angles
-		#angles=robot.JointOffset([i[0],i[1],0,i[2],i[3],0,0])
-
-		# Estimate the real position
-		#pos_r=get_position(Te(angles[0],angles[1],angles[2],angles[3],angles[4],angles[5],angles[6]))
-
-		# Append the real estimation
-		#input_training_data_r=np.append(input_training_data_r, np.array([pos_r]), axis=0)
-
-		# Estimate the prediction
-		prediction=forwardModel.predict(np.array([i]))
-
-		# Append the network estimation
-		print index
-		print input_training_data_n[index]
-		input_training_data_n=np.append(input_training_data_n[index], prediction-p_cubes[index], axis=0)
-"""
-#print input_training_data_n
-
-#input_training_data_n = np.concatenate(input_training_data_n)
-print input_training_data_n[0].shape
 # Plot the trajectories
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# Make data
-X = input_training_data_n[:,[0]]
-Y = input_training_data_n[:,[1]]
-Z = input_training_data_n[:,[2]]
+for data in input_training_data_n: 
+    X = -data[:,0]
+    Y = -data[:,1]
+    Z = -data[:,2]
 
-# Make the scatter plot
-surf = ax.scatter(X, Y, Z)
+    surf = ax.scatter(X, Y, Z)
 
-# Show the scatter plot
-# plt.show()
+plt.show()
 
 
 # Define the Inverse Model
-InverseModel=Sequential()
-
-# Add the input Layer and the 1rst Hidden Layer
-InverseModel.add(Dense(200,input_shape=(6,),init="uniform",activation="sigmoid"))
-
-# Add a second Hidden Layer
-InverseModel.add(Dense(200,init="uniform",activation="sigmoid"))
-
-# Add the Output Layer
+InverseModel = Sequential()
+InverseModel.add(Dense(200, activation="sigmoid", kernel_initializer="uniform", input_shape=(6,)))
+InverseModel.add(Dense(200, activation="sigmoid", kernel_initializer="uniform"))
 InverseModel.add(Dense(5))
-
-# Define the Training Process
 InverseModel.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
 
 # Define the final input and output data
-input_training_data=input_training_data_n[:-1]
-output_training_data=output_training_data[1:]
-input_training_data_t = np.vstack((input_training_data_n[0], input_training_data_n[:-2]))
-input_training_combined = np.hstack((input_training_data, input_training_data_t))
+input_training_data = input_training_data_n[:,:-1]
+out_tr_dat = out_tr_dat[:,1:]
 
+input_training_data_t = np.concatenate((input_training_data_n[:,[0]], input_training_data_n[:,:-2]), axis=1)
+
+# Put matrices side by side
+input_training_combined = np.concatenate((input_training_data, input_training_data_t), axis=2)
+
+out_tr_dat = np.concatenate(out_tr_dat)
+input_training_combined = np.concatenate(input_training_combined)
 
 # Train the Model
-InverseModel.fit(input_training_combined,output_training_data,batch_size=1)
+history = InverseModel.fit(input_training_combined,out_tr_dat,batch_size=1, epochs=1)
 
 # Save the Model
 InverseModel.save(inverse_model_file)
+def plotperformance(history):
+    range_epochs = range(0, len(history[history.keys()[0]]))
+    plt.figure()
+    plt.title("performance")
+    plt.xlabel("epoch")
+    plt.ylabel("loss/accuracy")
+    plt.ylim(0, 1)
+    for res in history.keys():
+        plt.plot(range_epochs, history[res], label=res)
+    plt.legend(loc='upper right')
+    plt.show()
+
+plotperformance(history.history)
 
 
+if False != False: # ;), if X_Test  and y_Test are defined this will plot receiver operator characteristic
+    scores = model.evaluate(X_Test, y_Test)
+    print scores
 
+    from sklearn.metrics import roc_curve, auc
+    fpr, tpr, _ = roc_curve(y_Test, model.predict(X_Test))
+                        
+    roc_auc = auc(fpr, tpr)
 
-	 
-	 
+    plt.plot(fpr,tpr,color='darkorange',label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.legend(loc="lower right")
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+    plt.show()
+
